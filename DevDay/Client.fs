25eacc7af55d5c256bffd64e0bbbee8dc2932a5c
Jonas Juselius <jonas.juselius@itpartner.no>
2016-11-09 13:51:15 +0100

    Final, persistent, styled, reactive version.

--- DevDay/Client.fs
+++ DevDay/Client.fs
@@ -1,21 +1,85 @@
 namespace DevDay
 
 open WebSharper
-open WebSharper.JavaScript
-open WebSharper.UI.Next
-open WebSharper.UI.Next.Client
-open WebSharper.UI.Next.Html
 
 [<JavaScript>]
 module Client =
+    open WebSharper.JavaScript
+    open WebSharper.UI.Next
+    open WebSharper.UI.Next.Client
+    open WebSharper.UI.Next.Html
+
+    let private todoList = ListModel.Create (fun (n, _) -> n) []
+
+    let private initTodo () =
+        async {
+            let! items = Server.getTodoItems ()
+            items
+            |> Array.iter (fun x ->
+                todoList.Add (x.Id, x.TodoItem)
+            )
+        } |> Async.Start
+
+    let private addTodoItem s =
+        async {
+            let! id = Server.addTodoItem s
+            todoList.Add (id, s)
+        } |> Async.Start
+
+    let private rmTodoItem id =
+        Server.rmTodoItem id |> Async.Start
+        todoList.RemoveByKey id
+
+    let private special (s: string)=
+        match s with
+        | x when x.StartsWith "NB!" -> "alert alert-danger"
+        | x when x.StartsWith "Important:" -> "alert alert-warning"
+        | x when x.StartsWith "Note:" -> "alert alert-notice"
+        | x when x.StartsWith "Done:" -> "alert alert-success"
+        | _ -> "alert"
 
     let Main () =
-        let msg = Var.Create ""
-        async {
-            let! x = Server.helloWorld ()
-            msg.Value <- x
-        } |> Async.Start
+        initTodo ()
+        let todoText = Var.Create ""
+        let tagIt = Submitter.Create todoText.View ""
+        let todoTagged = tagIt.View.MapAsync (fun x ->
+            match x with
+            | "" -> async {return ""}
+            | s -> Server.tagWithDateTime s
+            )
+        let submitTodo = Submitter.Create todoTagged ""
+        submitTodo.View |> View.Sink (function
+            | "" -> ()
+            | x -> addTodoItem x
+        )
         div [
-            h1 [text "To be written"]
-            Doc.TextView msg.View
+            Doc.Input [] todoText
+            Doc.Button "Tag" [attr.``class`` "btn"] tagIt.Trigger
+            h5Attr [attr.classDyn (todoText.View.Map special)] [textView todoText.View]
+            hr []
+            divAttr [attr.``class`` "jumbotron"] [
+                h3 [text "Preview:"]
+                h4 [textView todoTagged]
+            ]
+            Doc.EmbedView <| tagIt.View.Map (function
+                | "" -> Doc.Empty
+                | _ -> Doc.Button "Add" [attr.``class`` "btn btn-primary"] submitTodo.Trigger :> Doc
+                )
+            h2 [text "Todo items"]
+            tableAttr [attr.``class`` "table"] [
+                tbody [
+                    todoList.View
+                    |> Doc.BindSeqCached (fun (id, x) ->
+                        tr [
+                            tdAttr [attr.``class`` <| special x] [text <| string id + " " + x]
+                            td [
+                                buttonAttr [
+                                    attr.``class`` "btn btn-danger"
+                                    on.click (fun _ _ -> rmTodoItem id)
+                                ] [spanAttr [attr.``class`` "glyphicon glyphicon-minus"] []]
+                            ]
+                        ]
+                    )
+                ]
+            ]
         ]
--- DevDay/Remoting.fs
+++ DevDay/Remoting.fs
@@ -3,6 +3,14 @@ namespace DevDay
 open WebSharper
 
 module Server =
+    open Microsoft.FSharp.Data.TypeProviders
+
+    type Db = SqlDataConnection<ConnectionString = "Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=Todo;Integrated Security=True;">
+
+    type TodoItem = {
+        Id: int
+        TodoItem: string
+    }
 
     [<Rpc>]
     let helloWorld () =
@@ -10,3 +18,43 @@ module Server =
             return "Hello world"
         }
 
+    [<Rpc>]
+    let tagWithDateTime str =
+        let str' = sprintf "%s [%A]" str System.DateTime.Now
+        async {
+            return str'
+        }
+
+    [<Rpc>]
+    let getTodoItems () =
+        let db = Db.GetDataContext ()
+        let items =
+            query {
+                for x in db.Item do
+                select x
+            }
+            |> Array.ofSeq
+            |> Array.map (fun x ->
+                {Id = x.Id; TodoItem = x.TodoItem}
+            )
+        async { return items }
+
+    [<Rpc>]
+    let addTodoItem s =
+        let db = Db.GetDataContext ()
+        let i = Db.ServiceTypes.Item (TodoItem = s)
+        i |> db.Item.InsertOnSubmit
+        db.DataContext.SubmitChanges ()
+        async {return i.Id}
+
+    [<Rpc>]
+    let rmTodoItem id =
+        let db = Db.GetDataContext ()
+        db.DataContext.SubmitChanges ()
+        query {
+            for x in db.Item do
+            where (x.Id = id)
+            select x
+        } |> db.Item.DeleteAllOnSubmit
+        db.DataContext.SubmitChanges ()
+        async {return ()}
